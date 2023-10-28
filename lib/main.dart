6156dde19649376896d1f195fee5e91f10abc67c
pankaj104 <pankajkumar1122111@gmail.com>
2023-10-28 16:45:42 +0530

    frist commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+          create: (BuildContext context) {  },
+          child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: ...,
+      ),
+    );
+  }
+}
